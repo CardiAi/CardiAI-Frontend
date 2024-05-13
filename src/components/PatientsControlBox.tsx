33f6d6f5af9181b5f579dda9e032a70ab011bd77
ahmed el sayad <poppop7517@gmail.com>
2024-05-14 02:30:18 +0300

    optimizing search

--- src/components/PatientsControlBox.tsx
+++ src/components/PatientsControlBox.tsx
@@ -13,10 +13,6 @@ function PatientsControlBox() {
   const debounced = useDebouncedCallback((e: ChangeEvent<HTMLInputElement>) => {
     searchParams.set("search", e.target.value);
     setSearchParams(searchParams);
-    if (e.target.value === "") {
-      searchParams.delete("search");
-      setSearchParams(searchParams);
-    }
   }, 350);
 
   return (
